e83b5a87224da9754beac009621efa6bde2f7a38
lpelkey <85022999+lpelkey@users.noreply.github.com>
2026-03-25 10:00:03 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: '10mb'}));
 
 app.post('/api', async (req, res) => {
   try {
@@ -23,4 +23,5 @@ app.post('/api', async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log('Astra server running'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('Astra running on port ' + PORT));
